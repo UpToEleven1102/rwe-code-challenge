e2df4794887b2b1755a1a9f3184038de2bff5518
Andrew Vu <andrew.vu@drivetime.com>
2022-10-05 15:37:50 -0500

    wip: integrating wind-park-api into docker compose

--- windpark-app/app.js
+++ windpark-app/app.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 
 const amqp = require('amqplib/callback_api');
+const WindParkApiService = require("./services/wind-park-api");
 
 const queue = process.env.QUEUE_NAME;
 const amqpUrl = process.env.AMQP_URL || 'amqp://localhost';
 
 function main() {
+    // initialize service to frequently fetch data from API
+    console.log('Init Wind park service...')
+    const windParkService = new WindParkApiService();
+    windParkService.init();
+    console.log('Initialized wind park service...');
+
+
+    // connect to rabbitMQ to send data to the queue
     amqp.connect(amqpUrl, function(error0, connection) {
         if (error0) {
             throw error0;
@@ -20,10 +29,11 @@ function main() {
             });
 
             setInterval(() => {
-                const msg = "Sending message at " + Date.now();
+                const data = windParkService.getAggregateValues()
+                data.sent_at = Date.now();
+                const msg = JSON.stringify(data);
                 channel.sendToQueue(queue, Buffer.from(msg));
-                console.log(" [x] Sent %s", msg);
-            }, 1000);
+            }, 5000);
         });
     });
 }
---
+++ windpark-app/services/wind-park-api.js
@@ -0,0 +1,91 @@
+const axios = require('axios');
+const windParkApiUrl = process.env.WIND_PARK_API_URL;
+const retrieveDataInterval = process.env.RETRIEVE_DATA_INTERVAL || 1000; // default to 1 second
+const aggregationTimeInterval = process.env.AGGREGATION_TIME_INTERVAL || 5 * 60000; // default to 5 minutes
+
+module.exports = class WindParkApiService {
+    _aggregateData = [{recorded_timestamp: Date.now(), data: []}];
+    _axiosClient;
+
+    constructor() {
+        this._axiosClient = axios.create({
+            baseURL: windParkApiUrl
+        });
+    }
+
+    async _retrieveData() {
+        const data = await this.getAllSites();
+        this._aggregateData[this._aggregateData.length - 1].data.push(data);
+    }
+
+
+    // Service public methods:
+    /**
+     * This function initializes the process of retrieving data from API every second
+     */
+    init() {
+        setInterval(() => this._retrieveData(), retrieveDataInterval);
+        // push new record every 5 minutes
+        setInterval(() => this._aggregateData.push({
+            recorded_timestamp: Date.now(),
+            data: []
+        }), aggregationTimeInterval);
+    }
+
+    getAggregateValues() {
+        // aggregate the latest record into a summary report
+        const {
+            recorded_timestamp,
+            data: sitesData
+        } = JSON.parse(JSON.stringify(this._aggregateData[this._aggregateData.length - 1]));
+        const sites = {};
+
+        for (let record of sitesData) {
+            for (let site of record) {
+                const _site = sites[site.Id];
+                if (!_site) {
+                    site.Turbines = site.Turbines.map(turbine => ({...turbine, count: 1}))
+                    sites[site.Id] = site;
+                } else {
+                    for (let turbine of site.Turbines) {
+                        const _turbine = _site.Turbines.filter(val => val.id === turbine.id);
+                        if (_turbine) {
+                            _turbine.CurrentProduction += turbine.CurrentProduction;
+                            _turbine.Windspeed += turbine.Windspeed;
+                            _turbine.count++;
+                        } else {
+                            _site.Turbines.push({...turbine, count: 1});
+                        }
+                    }
+                }
+            }
+        }
+
+        let sitesResponse = Object.values(sites).map(site => {
+            site.Turbines = site.Turbines.map(turbine => ({
+                id: turbine.Id,
+                name: turbine.Name,
+                manufacturer: turbine.Manufacturer,
+                maxProduction: turbine.MaxProduction,
+                AvgCurrentProduction: turbine.CurrentProduction / turbine.count,
+                AvgWindSpeed: turbine.Windspeed / turbine.count
+            }));
+            return site;
+        });
+
+        return {
+            recorded_since: recorded_timestamp,
+            sites: sitesResponse
+        }
+    }
+
+    async getAllSites() {
+        const res = await this._axiosClient.get('Site');
+        return res.data;
+    }
+
+    async getSiteById(id) {
+        const res = await this._axiosClient.get(`Site/${id}`);
+        return res.data;
+    }
+}
